ea8b991621f3a860dac33907b89a03a42de62ce9
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-13 22:59:28 +0200

    create filter input for our contacts list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import ContactList from './components/contact-list/contact-list.component';
-import contactsData from './data/contacts.data';
+import React, { useState } from "react";
+import ContactList from "./components/contact-list/contact-list.component";
+import contactsData from "./data/contacts.data";
 
 function App() {
-  const [contacts , setContacts] = useState(contactsData)
+  const [contacts, setContacts] = useState(contactsData);
 
-  const removeContact = (id)=>{
-    setContacts(contacts.filter(contact => contact.id !== id))
-  }
+  const removeContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
   return (
-   <ContactList removeContact={removeContact} contacts={contacts} />
+     <ContactList removeContact={removeContact} contacts={contacts} />
   );
 }
 
--- src/components/contact-list/contact-list.component.jsx
+++ src/components/contact-list/contact-list.component.jsx
@@ -1,26 +1,50 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
 const ContactList = ({ contacts, removeContact }) => {
+  const [query, setQuery] = useState("");
+
+  const showingContacts = query ===''
+  ? contacts
+  : contacts.filter(contact => contact.name.toLowerCase().includes(query.toLowerCase()) )
 
   return (
-    <ol className="contact-list">
-      {contacts.map(({ id, name, avatarURL, handle }) => (
-        <li key={id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{ backgroundImage: `url(${avatarURL})` }}
-          ></div>
-          <div className="contact-details">
-            <p>{name}</p>
-            <p>@{handle}</p>
-          </div>
-          <button
-            className="contact-remove"
-            onClick={() => removeContact(id)}>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
+    <div className="list-contacts">
+      <div className="list-contacts-top">
+        <input
+          className="search-contacts" 
+          type="text"
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event)=> setQuery(event.target.value.trim())} />
+      </div>
+      <ol className="contact-list">
+        {showingContacts.map(({ id, name, avatarURL, handle }) => (
+          <li key={id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${avatarURL})` }}
+            ></div>
+            <div className="contact-details">
+              <p>{name}</p>
+              <p>@{handle}</p>
+            </div>
+            <button
+              className="contact-remove"
+              onClick={() => removeContact(id)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
